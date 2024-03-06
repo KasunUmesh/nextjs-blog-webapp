e4e4c8705e976711f629a6efe6c671f1da701d8e
Kasun Umesh <kasunumesh38@gmail.com>
2024-03-06 16:06:48 +0530

    contact page create

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,21 @@
+import Styles from "./contact.module.css"
+import Image from "next/image"
 const ContactPage = () => {
     return (
-        <div>ContactPage</div>
+        <div className={Styles.container}>
+            <div className={Styles.imgContainer}>
+                <Image src="/contact.png" alt="contact" fill className={Styles.img}/>
+            </div>
+            <div className={Styles.formContainer}>
+                <form action="" className={Styles.form}>
+                    <input type="text" placeholder="Name and Surname" />
+                    <input type="text" placeholder="Email Address" />
+                    <input type="text" placeholder="Phone Number (Optional)" />
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button>Sent</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
