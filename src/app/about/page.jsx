d54ac364c4fd32bab6059452ba8f25fd4dd94a7a
Kasun Umesh <kasunumesh38@gmail.com>
2024-03-06 15:48:27 +0530

    about section created

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,39 @@
-import Image from "next/image" 
-import Styles from "./about.module.css"
+import Image from "next/image";
+import Styles from "./about.module.css";
 const AboutPage = () => {
-    return (
-        <div>
-            <div className={Styles.imgContainer}>
-                <Image src="/about.png" alt="about" fill/>
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.textContainer}>
+        <h2 className={Styles.subtitle}>About Agency</h2>
+        <h1 className={Styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={Styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={Styles.boxes}>
+            <div className={Styles.box}>
+                <h1>10 K+</h1>
+                <p>Year of experience</p>
             </div>
-            
+            <div className={Styles.box}>
+                <h1>10 K+</h1>
+                <p>Year of experience</p>
+            </div>
+            <div className={Styles.box}>
+                <h1>10 K+</h1>
+                <p>Year of experience</p>
+            </div>
         </div>
-    )
-}
+      </div>
+      <div className={Styles.imgContainer}>
+        <Image src="/about.png" alt="about" fill className={Styles.img}/>
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.logo}></div>
+            <div className={styles.logo}>Logo</div>
             <div className={styles.text}>
                 Kasun creative thoughts agency © All rights reserved.
             </div>
